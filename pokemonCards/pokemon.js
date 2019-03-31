b76e3354578ae548b0329ca2f8d59847485d9b84
John Biederman <squirrelmaster17@gmail.com>
2019-03-31 16:08:38 -0600

    added 25 pokeon to js and wrote in main js

---
+++ pokemonCards/pokemon.js
@@ -0,0 +1,23 @@
+import { pokemonData } from '../pokemonCards/pokemonData.js'
+ console.log(pokemonData)
+pokemon.forEach((singlePoke) => {
+fetch(singlePoke.url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+    createPokeCard(myJson)
+  })
+})
+  const mainContainer = document.queryselector('container') 
+
+  function createPokeCard(pokeData) {
+      let card = document.createElement('div')
+      let name = document.createElement('p')
+
+      name.textContent = pokeData.name
+      card.appendchild(name)
+      mainContainer.appendchild(card)
+
+  }
