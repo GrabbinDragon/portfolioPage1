4d6d33ba7d319c604fe30b38bc88a34555d42ab0
John Biederman <squirrelmaster17@gmail.com>
2019-04-26 22:13:22 -0600

    more guano bowls

--- pokemonCards/pokemon.js
+++ pokemonCards/pokemon.js
@@ -123,4 +123,4 @@ specificPokemonButton.addEventListener('click', function() {
   let pokemonID = prompt('Enter an ID of an existing pokemon (1-721):')
   fetchSinglePokemon(pokemonID)
 })
-//Guano bowls, collect the whole set!!
+//Guano bowls, collect the whole set!!!
